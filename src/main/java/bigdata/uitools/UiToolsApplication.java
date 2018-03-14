da2c9cb5e71166021edadf4dc38209376c272f22
wangjc2018 <771203240@qq.com>
2018-03-14 18:21:36 +0800

    初始化

---
+++ src/main/java/bigdata/uitools/UiToolsApplication.java
@@ -0,0 +1,12 @@
+package bigdata.uitools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UitoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UitoolsApplication.class, args);
+	}
+}
